3c7e669efe950563e37f50e3a453b7c1cadaf084
patrick-putz <Putz.Patrick.1996@gmail.com>
2022-12-08 10:41:18 +0100

    Reverted Stack implementation

--- src/A01_Stack/Stack.java
+++ src/A01_Stack/Stack.java
@@ -3,53 +3,30 @@ package A01_Stack;
 
 public class Stack<T>
 {
-	 private Node<T> first;
-     private int counter;
+    private Node<T> first;
     /**
      * Oberstes Element entfernen und zurückliefern.
      * Existiert kein Element, wird eine Exception ausgelöst.
-     * @throws StackEmptyException 
+     * @throws StackEmptyException
      */
     public T pop() throws StackEmptyException {
-        if (first == null)
-            throw new StackEmptyException();
 
-        Node<T> result = first;
-        first = first.getNext();
-        counter--;
-        result.setNext(null);
-        return result.getData();
+        return null;
     }
-    
+
     /**
      * Übergebenen T auf Stack (als oberstes Element) speichern.
      * @param i data
      */
     public void push(T i) {
-        Node<T> newElement = new Node<>(i);
 
-        newElement.setNext(first);
-        first = newElement;
-        counter++;
     }
-    
+
     /**
      * Liefert die Anzahl der Elemente im Stack
      * @return
      */
     public int getCount() {
-        return counter;
-    }
-
-    public int getCountAlternative() {
-        int counter = 0;
-        Node<T> iterator = first;
-
-        while (iterator != null) {
-            counter++;
-            iterator = iterator.getNext();
-
-        }
-        return counter;
+        return 0;
     }
 }
